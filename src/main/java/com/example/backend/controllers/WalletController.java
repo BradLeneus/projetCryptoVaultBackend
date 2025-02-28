f9b3158f55393fea3c1d6f048d63d6f585252764
Samuel Robillard <6222321@collegemv.qc.ca>
2025-02-28 15:41:53 -0500

    rechercher le wallet par id du user

--- src/main/java/com/example/backend/controllers/WalletController.java
+++ src/main/java/com/example/backend/controllers/WalletController.java
@@ -1,14 +1,13 @@
 package com.example.backend.controllers;
 
+import com.example.backend.model.Customer;
 import com.example.backend.model.Wallet;
 import com.example.backend.repositories.WalletRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/wallet")
@@ -23,5 +22,10 @@ public class WalletController {
 
         return walletRepository.findAll();
 
+    }
+
+    @GetMapping("/getbyuser/{id}")
+    public List<Wallet> getByIdUser(@PathVariable int id){
+      return  walletRepository.findAllByCustomer_Id(id);
     }
 }
--- src/main/java/com/example/backend/repositories/WalletRepository.java
+++ src/main/java/com/example/backend/repositories/WalletRepository.java
@@ -4,5 +4,10 @@ package com.example.backend.repositories;
 import com.example.backend.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WalletRepository extends JpaRepository<Wallet, Integer> {
+
+    public List<Wallet> findAllByCustomer_Id(int id);
 }
+
